cb669f10d700f29dbe471c3d8abb25edff0f79d3
Max Chopart <chopart.max@proton.me>
2025-02-27 13:10:35 +0100

    [Upd] Improved annotation script to choose input and output files by folder

--- bin/annotate-data.sh
+++ bin/annotate-data.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
-INPUT_FILE_NAME=loss-scenarios.tsv
-INPUT_FILE=./data/AIDA/$INPUT_FILE_NAME
-
-if [ ! "$#" -eq 0 ]; then
-	echo Transforms STPA from $INPUT_FILE to ttl
-        echo Usage :  $0
-        echo Example:
-        echo "  $0 "
-        exit
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 BASE_DIRECTORY"
+    echo "Example:"
+    echo "  $0 /path/to/base"
+    exit 1
 fi
 
+BASE_DIRECTORY=$1
+INPUT_FILE_NAME="loss-scenarios.tsv"
+INPUT_FILE="$BASE_DIRECTORY/$INPUT_FILE_NAME"
+OUTPUT_DIRECTORY="./target/$(basename $BASE_DIRECTORY)"
+OUTPUT_FILE="$OUTPUT_DIRECTORY/annotated-scenarios.ttl"
 
-FUNCTION_ID=transform-data
-
-SPIPES_SERVICE=http://172.25.48.1:8080/s-pipes
-
+FUNCTION_ID="transform-data"
+SPIPES_SERVICE="http://172.25.48.1:8080/s-pipes"
 DIR="$(dirname $(realpath -s $0))"
-
-OUTPUT_FILE=./target/AIDA/annotated-scenarios.ttl
 
 echo "==================================="
 echo "INFO: s-pipes service url $SPIPES_SERVICE"
 echo "INFO: function id $FUNCTION_ID"
 echo "INFO: input file $INPUT_FILE"
+echo "INFO: output directory $OUTPUT_DIRECTORY"
 echo "==================================="
 
-cd $DIR/../$DATASET_TYPE
-mkdir -p ./target
+cd "$DIR/../$DATASET_TYPE"
+mkdir -p "$OUTPUT_DIRECTORY"
 
 URL="$SPIPES_SERVICE/service?_pId=$FUNCTION_ID&datasetResource=@$INPUT_FILE_NAME"
-INPUT_FILE_ABSOLUTE="`realpath $INPUT_FILE`"
+INPUT_FILE_ABSOLUTE="$(realpath $INPUT_FILE)"
 
 set -x
 set -e
 
 # TODO use --fail-with-body when having curl 7.76; # rerun without --fail to see the error from the server
-curl --fail --location --request POST  \
-	--header 'Accept: text/turtle' \
-	--form 'files=@"'${INPUT_FILE_ABSOLUTE}'"' \
-	"$URL" > $OUTPUT_FILE
+curl --fail --location --request POST \
+    --header 'Accept: text/turtle' \
+    --form "files=@\"${INPUT_FILE_ABSOLUTE}\"" \
+    "$URL" > "$OUTPUT_FILE"
 
 set +x
 
