b66a8899d673f5b27f1514eb46ed7ab0ee93b9bf
Max Chopart <chopart.max@proton.me>
2025-03-13 11:07:59 +0100

    [Upd] Add I/O directory parameter to scripts

--- bin/transform-vocabulary.sh
+++ bin/transform-vocabulary.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-if [ ! "$#" -eq 0 ]; then
-	echo Transforms reliability vocabulary from reliability-aviation-profile-components.ttl file to ttl
-        echo Usage :  $0
-        echo Example:
-        echo "  $0 "
-        exit
+# Check if the output directory is provided as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Transforms reliability vocabulary from reliability-aviation-profile-components.ttl file to ttl"
+    echo "Usage: $0 <output_directory>"
+    echo "Example:"
+    echo "  $0 ./target"
+    exit 1
 fi
 
+OUTPUT_DIR=$1
+
 FUNCTION_ID=transform-vocabulary
-
 SPIPES_SERVICE=http://172.25.48.1:8080/s-pipes
-
 DIR="$(dirname $(realpath -s $0))"
-
-OUTPUT_FILE=./target/vocabulary.ttl.txt
+OUTPUT_FILE=$OUTPUT_DIR/vocabulary.ttl.txt
 
 echo "==================================="
 echo "INFO: s-pipes service url $SPIPES_SERVICE"
 echo "INFO: function id $FUNCTION_ID"
+echo "INFO: output directory $OUTPUT_DIR"
 echo "==================================="
 
-mkdir -p ./target
+mkdir -p "$OUTPUT_DIR"
 
 URL="$SPIPES_SERVICE/service?_pId=$FUNCTION_ID"
 
@@ -30,8 +31,7 @@ set -e
 
 # TODO use --fail-with-body when having curl 7.76; # rerun without --fail to see the error from the server
 curl --fail --location --request GET \
-	--header 'Accept: text/turtle' \
-	"$URL" > $OUTPUT_FILE
+    --header 'Accept: text/turtle' \
+    "$URL" > "$OUTPUT_FILE"
 
 set +x
-
